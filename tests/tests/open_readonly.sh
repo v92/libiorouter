3835d36264e0bb34d1f5237493f5fdfbdd0c1fca
Tomas Corej <tomas.corej@websupport.sk>
2015-05-31 18:01:15 +0200

    fix tests

--- tests/tests/open_readonly.sh
+++ tests/tests/open_readonly.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-tempc=`mktemp`
+ROOTDIR=${PWD%*/*/*}
+TESTDIR=$ROOTDIR/tests
 
 setUp() {
-if [ ! -f ../nfsmnt/test.php ]; then
-	dd if=/dev/urandom of=../nfsmnt/test.php count=10
+if [ ! -f $TESTDIR/nfsmnt/test.php ]; then
+	dd if=/dev/urandom of=$TESTDIR/nfsmnt/test.php count=10
 fi
-echo	"
-#include <stdio.h>
+cc -ggdb -o $TESTDIR/tests/open_readonly open_readonly.c
 
-int main(void)
-{
-FILE *fp;
-fp = fopen(\"${PWD%*/*}/nfsmnt/test.php\",\"r\");
-fclose(fp);
-}
-" > ${tempc}.c
-cc -o ${tempc} ${tempc}.c
-
-if [ ! -f ../../libiorouter.so ]; then 
-	( cd ../.. && make)
+if [ ! -f $ROOTDIR/libiorouter.so ]; then 
+	( cd $ROOTDIR && make)
 fi
 }
 
 tearDown() {
-	rm -f ${tempc} ${tempc}.c 
+	:;
+	#echo rm -f $TESTDIR/tests/open_readonly
 }
 
 
 test_open_readonly() {
-LIBIOR_IO=on LIBIOR_REWRITEDIR=${PWD%*/*}/nfsmnt LIBIOR_CACHEDIR=${PWD%*/*}/run LD_PRELOAD=${PWD%*/*/*}/libiorouter.so ${tempc}
-test -f ../run/test.php
-assertEquals "$?" "0"
+LIBIOR_IO=on LIBIOR_REWRITEDIR=$TESTDIR/nfsmnt LIBIOR_CACHEDIR=$TESTDIR/run LD_PRELOAD=$ROOTDIR/libiorouter.so $TESTDIR/tests/open_readonly $TESTDIR/nfsmnt/test.php
+echo LIBIOR_IO=on LIBIOR_REWRITEDIR=$TESTDIR/nfsmnt LIBIOR_CACHEDIR=$TESTDIR/run LD_PRELOAD=$ROOTDIR/libiorouter.so strace -s 256 $TESTDIR/tests/open_readonly $TESTDIR/nfsmnt/test.php > $TESTDIR/tests/open_readonly.runstr
+
+cat << EOF >> $TESTDIR/tests/open_readonly.runstr
+set environment LIBIOR_IO=on
+set environment LIBIOR_REWRITEDIR=$TESTDIR/nfsmnt
+set environment LIBIOR_CACHEDIR=$TESTDIR/run
+set environment LD_PRELOAD=$ROOTDIR/libiorouter.so
+EOF
+
+test -f $TESTDIR/run/$TESTDIR/nfsmnt/test.php
+assertEquals "$TESTDIR/nfsmnt/test.php does not exist in $TESTDIR/run: " "0" "$?"
 }
 
 source "/usr/share/shunit2/shunit2"

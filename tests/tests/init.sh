39b001481142d96c4f28e2a110aa6b48c5c40bc0
Tomas Corej <tomas.corej@websupport.sk>
2015-06-14 18:00:17 +0200

    fix order of commands, LD_PRELOAD should be last so it would not interfer with other commands

--- tests/tests/init.sh
+++ tests/tests/init.sh
@@ -3,8 +3,9 @@ ROOTDIR=${PWD%*/*/*}
 TESTDIR=$ROOTDIR/tests
 CACHEFILE=$TESTDIR/run/${TESTDIR:1}/nfsmnt/test.php
 TESTFILE=$TESTDIR/nfsmnt/test.php
+export testfile_size=`stat -c %s $TESTFILE`
 export LIBIOR_MAXFILESIZE=10485760
 export LIBIOR_REWRITEDIR=$TESTDIR/nfsmnt
 export LIBIOR_CACHEDIR=$TESTDIR/run
+# LD_PRELOAD must be last
 export LD_PRELOAD=$ROOTDIR/libiorouter.so
-export testfile_size=`stat -c %s $TESTFILE`
